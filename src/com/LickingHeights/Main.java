90e06e536e8dfbdce45e53ac40ac1f9fb82fc9b4
Garrett Miller <garrett.miller.2020@student.lhschools.org>
2020-02-19 12:50:54 -0500

    intelij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,15 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("My name is ");
+        System.out.println("Garrett Miller");
+
+        //Print three more lines abut what you did last summer
+        System.out.println("Last summer I went to Philmont in New Mexico with my Boy Scout Troop on a 12 day backpacking trip.");
+        System.out.println("I also bought a brand new mountain bike last summer and learned how to mountain bike.");
+        System.out.println("The last big thing I did was go on a 10 day Carnival Cruise through the bahamas and got to see the beautiful ocean.");
+    }
+}
